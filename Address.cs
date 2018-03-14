6e078c4dcef1316b181d86756d2e42b3bd7a64e5
erasmuss22 <erasmuss22@gmail.com>
2018-03-13 21:21:35 -0500

    created first test scenario

---
+++ Address.cs
@@ -0,0 +1,11 @@
+namespace DataDriven
+{
+    public class Address
+    {
+        public string Address1 { get; set; }
+        
+        public string Address2 { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DataDriven
 {
@@ -6,7 +8,68 @@ namespace DataDriven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DynamicClass addressCLass = new DynamicClass()
+            {
+                ObjectName = "Address",
+                Fields = new List<Field>()
+                {
+                    new Field() { PropertyName = "Address1", PropertyType = typeof(string) },
+                    new Field() { PropertyName = "Address2", PropertyType = typeof(string) },
+                    new Field() { PropertyName = "PostalCode", PropertyType = typeof(string) }
+                }
+            };
+
+            DynamicClass dynamicClass = new DynamicClass()
+            {
+                ObjectName = "Person",
+                Fields = new List<Field>()
+                {
+                    new Field() { PropertyName = "FirstName", PropertyType = typeof(string) },
+                    new Field() { PropertyName = "LastName", PropertyType = typeof(string) },
+                    new Field() { PropertyName = "Age", PropertyType = typeof(int) },
+                    new Field() { PropertyName = "Address", DynamicRuntimeType = "Address" }
+                }
+            };
+
+            DynamicClassInitializer.InitializeDynamicClasses(new List<DynamicClass>() { addressCLass, dynamicClass });
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 50000000; i++) {
+                Person person = new Person();
+                person.FirstName = "John";
+
+                Address address = new Address();
+                address.Address1 = "1234 Test St";
+                person.Address = address;
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("Class Instantiation Elapsed time {0} ms",stopWatch.ElapsedMilliseconds);
+
+            stopWatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 50000000; i++) {
+                BaseObject address = DynamicClassInitializer.CreateObjectInstanceByName("Address");
+
+                Type addressType = address.GetType();
+                var address1 = addressType.GetProperty("Address1");
+
+                // set a value on the address's Address1 property
+                address1.SetValue(address, "1234 Test St");
+
+                // create a new Person
+                BaseObject person = DynamicClassInitializer.CreateObjectInstanceByName("Person");
+
+                Type personType = person.GetType();
+                var addressProperty = personType.GetProperty("Address");
+
+                // set the dynamically created Address on the dynamically created Person's Address property
+                addressProperty.SetValue(person, address);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("Dynamic Instantiation Elapsed time {0} ms",stopWatch.ElapsedMilliseconds);
         }
     }
 }
